45d67aef047714d35a1d78249e2e8b51f4918aad
Jhonnier Sánchez <jhsanchez@conexia.com>
2021-08-11 22:02:02 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cnx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'conexia-login-lib';
+}
